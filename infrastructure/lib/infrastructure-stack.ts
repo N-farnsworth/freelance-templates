5700d581d423eaf51c54dc32c06be673bbf476c7
Nicholas Farnsworth <nich.farnsworth@gmail.com>
2026-06-30 11:32:35 -0700

    Add CDK Service Configuration

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -4,6 +4,7 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -36,6 +37,32 @@ export class InfrastructureStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, 'Cluster', {
       clusterName: 'freelance-templates-cluster-cdk',
       vpc,
+    });
+
+    const existingRepo = ecr.Repository.fromRepositoryName(this, 'ExistingRepo', 'node-typescript-ci');
+
+    const service = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      cluster: cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(existingRepo, 'latest'),
+        containerPort: 3000,
+        logDriver: ecs.LogDrivers.awsLogs({ 
+          logGroup: logGroup,
+          streamPrefix: 'ecs', 
+        }),
+        environment: {
+          PORT: '3000',
+        },
+      },
+      desiredCount: 1,
+      publicLoadBalancer: true,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      assignPublicIp: true,
+    });
+
+    service.targetGroup.configureHealthCheck({
+      path: '/health',
     });
 
   }
