7ca1df879342344838a026d82fcc7860d3bd7f89
Nicholas Farnsworth <nich.farnsworth@gmail.com>
2026-06-30 13:47:16 -0700

    Add CDK Monitoring and Alerting

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -5,6 +5,11 @@ import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as snsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as cloudwatchActions from 'aws-cdk-lib/aws-cloudwatch-actions';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -65,5 +70,32 @@ export class InfrastructureStack extends cdk.Stack {
       path: '/health',
     });
 
+    const alertTopic = new sns.Topic(this, 'AlertTopic', {
+      topicName: 'service-alerts',
+    });
+
+    alertTopic.addSubscription(
+      new snsSubscriptions.EmailSubscription('nich.farnsworth@gmail.com')
+    );
+
+    const http5xxAlarm = new cloudwatch.Alarm(this, 'Http5xxAlarm', {
+      metric: service.loadBalancer.metrics.httpCodeElb(
+        elbv2.HttpCodeElb.ELB_5XX_COUNT
+      ),
+      threshold: 1,
+      evaluationPeriods: 2,
+      alarmDescription: 'ALB returned 5xx errors',
+    });
+
+    http5xxAlarm.addAlarmAction(new cloudwatchActions.SnsAction(alertTopic));
+
+    const unhealthyAlarm = new cloudwatch.Alarm(this, 'UnhealthyHostAlarm', {
+      metric: service.targetGroup.metrics.unhealthyHostCount(), 
+      threshold: 1,
+      evaluationPeriods: 2,
+      alarmDescription: 'Unhealthy targets detected'  
+    });
+
+    unhealthyAlarm.addAlarmAction(new cloudwatchActions.SnsAction(alertTopic));
   }
 }
